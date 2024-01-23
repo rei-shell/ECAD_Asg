6121e40715e173b3e23b52ea5384efc2be37f107
wcnjing <wenjinx2005@gmail.com>
2024-01-24 01:56:20 +0800

    added nav bar

---
+++ Header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gifted Treasures Gift Store</title>
+    <!-- Lastest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Link to compiled Bootstrap Javascript downloaded -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <!-- site specific cascading stylesheet -->
+    <link rel="stylesheet" href="css/site.css">
+</head>
+
+
+<body>
+    <div class="container-fluid">
+        <!-- 1st Row -->
+        <!-- 2nd Row -->
+        <div class="row">
+            <div class="col-sm-12">
+                <?php include("navbar.php");?>
+            </div>
+        </div>
+        <!-- 3rd Row -->
+        <div class="row">
+            <div class="col-sm-12" style="padding:15px;">
+            <!-- Define customised content here -->
+            <!-- closing of this column and row is in footer.php -->
+    
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+// Detect the current session
+session_start();
+// Include the Page Layout header
+include("header.php"); 
+?>
+<img src="Images/logo.png" class="img-fluid" 
+     style="display:block; margin:auto;"/>
+
+<?php 
+// Include the Page Layout footer
+include("footer.php"); 
+?>
